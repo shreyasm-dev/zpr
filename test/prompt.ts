b9cb38357b4e1561397f729e56747b91e4588ca5
shreyasm-dev <62489066+shreyasm-dev@users.noreply.github.com>
2023-01-07 09:37:48 -0600

    Write some tests

---
+++ test/prompt.ts
@@ -0,0 +1,67 @@
+import Prompt from '../src/main';
+import { expect } from 'chai';
+
+describe('Prompt', () => {
+  it('should be a class', () => {
+    expect(Prompt).to.be.a('function');
+  });
+
+  it('should have a constructor', () => {
+    expect(new Prompt()).to.be.an('object');
+  });
+
+  [
+    'text',
+    'hostname',
+    'fullHostname',
+    'username',
+    'lastCommandStatus',
+    'cwd',
+    'cwdFromHome',
+    'historyEventNumber',
+    'yymmdd',
+    'time24',
+    'time12Meridian',
+    'time24Seconds',
+    'dd',
+    'mmddyy',
+    'colorOpen',
+    'colorClose',
+    'toString',
+    'export',
+  ].forEach((method) => {
+    it(`should have a ${method} method`, () => {
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      expect((new Prompt() as any)[method]).to.be.a('function');
+    });
+  });
+
+  const prompt = new Prompt()
+    .colorOpen('red')
+    .text('[')
+    .username()
+    .text('@')
+    .hostname()
+    .text('] ')
+    .colorClose()
+    .colorOpen('green')
+    .cwdFromHome()
+    .colorClose()
+    .colorOpen('blue')
+    .text(' $ ')
+    .colorClose();
+
+  it('should use toString correctly', () => {
+    expect(prompt.toString()).to.equal('%F{red}[%n@%m] %f%F{green}%~%f%F{blue} $ %f');
+  });
+
+  describe('export', () => {
+    it('should use export correctly', () => {
+      expect(prompt.export()).to.equal('PROMPT="%F{red}[%n@%m] %f%F{green}%~%f%F{blue} $ %f"');
+    });
+
+    it('should use export correctly with a custom variable', () => {
+      expect(prompt.export('TEST')).to.equal('TEST="%F{red}[%n@%m] %f%F{green}%~%f%F{blue} $ %f"');
+    });
+  });
+});
